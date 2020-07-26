697af7f45d07fc7d721d0a492379e164cdb6aaeb
waikinnwk <waikinnwk@gmail.com>
2020-07-26 15:51:44 +0800

    20200726

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
 
 from db import db
 from app import path_mapping
