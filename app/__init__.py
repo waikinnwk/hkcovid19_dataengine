8b849520f09f43709ac84c50cc4db28afb2418b5
waikinnwk <waikinnwk@gmail.com>
2020-07-26 15:58:52 +0800

    20200726 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
+
 
 
 from db import db
