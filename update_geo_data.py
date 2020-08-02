30dfb5f5a89bec4b984c874f289f7644ebd3b462
waikinnwk <waikinnwk@gmail.com>
2020-08-02 15:09:45 +0800

    commit - add new table 2

--- update_geo_data.py
+++ update_geo_data.py
@@ -1,4 +1,48 @@
-from service.save_data_to_db import *
+from app import db
+from data_obj.building_geo import *
+from data_obj.related_building import *
+import requests
+import ssl
+from datetime import datetime
+import time
 
 
-update_building_geo()
+def update_building_geo_sc():
+    print("update_building_geo start :" + time.strftime("%A, %d. %B %Y %I:%M:%S %p"))  
+    url_GetXY_Pre = "https://nominatim.openstreetmap.org/search?q="
+    url_GetXY_Post = "&format=json&polygon=1&addressdetails=1"
+    header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+    }    
+    data_obj = []
+    related_buildings = RelatedBuilding.query.order_by(RelatedBuilding.district).all()
+    for related_building in related_buildings:
+        trim_building_name = related_building.building_name.upper().replace("(NON-RESIDENTIAL)", "").strip()
+        building_geo_from_db = BuildingGeoInfo.query.filter(BuildingGeoInfo.district == related_building.district).filter(BuildingGeoInfo.building_name == trim_building_name).first()
+        if not building_geo_from_db :
+            try:
+                response = requests.get(url_GetXY_Pre+trim_building_name+","+related_building.district+url_GetXY_Post,data = data_obj, headers=header)
+                json_data = response.json()
+                for data in json_data:
+                    new_building_geo = BuildingGeoInfo(
+                        district=related_building.district,
+                        building_name = trim_building_name,
+                        lat = data["lat"],
+                        lon = data["lon"]
+                    )
+                    try:
+                        db.session.add(new_building_geo)
+                        db.session.commit()
+                        print('GEO inserted')    
+                        break
+                    except:
+                        print('DB except')    
+                time.sleep(1)
+            except:
+                print('Request except')   
+        else:
+            print('Skip record')
+    print("update_building_geo end :" + time.strftime("%A, %d. %B %Y %I:%M:%S %p"))  
+
+update_building_geo_sc()
