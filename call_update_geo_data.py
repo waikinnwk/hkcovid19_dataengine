7fb77abee3fd98615cbc54e1ebb971a74d11376c
waikinnwk <waikinnwk@gmail.com>
2020-08-02 16:19:20 +0800

    commit - update building geo data

---
+++ call_update_geo_data.py
@@ -0,0 +1,20 @@
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 5000
+
+
+from flask import url_for, request
+import requests
+
+url = "http://localhost:"+port+"/updateBuildingGEO"
+
+data_obj = {}
+response = requests.get(url,data = data_obj)
+json_data = response.json()
+
+print(json_data)
