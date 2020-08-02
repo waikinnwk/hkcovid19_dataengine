7c35fe5cf40673ccf77cb562292f5ec916b635fa
waikinnwk <waikinnwk@gmail.com>
2020-08-02 14:37:31 +0800

    commit - add new table 2

--- db/__init__.py
+++ db/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from data_obj import case, day_summary, related_building, building_geo
